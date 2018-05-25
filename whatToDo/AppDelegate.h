17cdf1810c5769cb01f06f74743da16badcb1ded
Alan Holguin <alanjholguin@gmail.com>
2018-05-25 11:21:29 -0400

    First commit

---
+++ whatToDo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  whatToDo
+//
+//  Created by C4Q on 5/25/18.
+//  Copyright © 2018 C4Q. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
